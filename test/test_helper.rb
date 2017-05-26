0251abccc3a1b12d92c1ae80f83b378a228b328b
Filipe Borges <filipebkc2209@gmail.com>
2017-05-26 19:05:15 -0300

    Finalized construction of static pages

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,8 @@
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+# @@@@@@@@@@@@@@@ Necessary to run unit test
+Minitest::Reporters.use!
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
